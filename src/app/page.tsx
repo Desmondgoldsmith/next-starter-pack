a0bf8d20e17404aee36e3f44b0f6d21053f363c1
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-24 23:35:15 +0000

    chore: setting things up xd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,12 @@ export default function Home() {
             </code>
             .
           </li>
-          <h1>Desmond Goldsmith</h1>
+
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <h1 className="text-3xl font-bold text-red-600">
+          Desmond Goldsmith The Best
+        </h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
