cd0d5028a33cb2d9fed1e637af51a556d177170f
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-25 00:24:44 +0000

    chore: testing  ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
           Desmond Goldsmith The Best !!!!
         </h1>
         <p className="text-red-600 text-2xl">Lets have fun!</p>
+        <p className="text-red-600 text-2xl">Again!</p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
